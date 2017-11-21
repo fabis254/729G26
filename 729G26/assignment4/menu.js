b1e69bf74b65afa71c08c73366df76f011493d20
Fabian Isaksson <fabis254@li3-10.ida.liu.se>
2017-11-21 16:42:20 +0100

    s

--- 729G26/assignment4/menu.js
+++ 729G26/assignment4/menu.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	var menu = $(
 		`<ul id="menu">
 			<li><div><a href="index.html">Home</a></div></li>
-			<li><div>Slideshow</div></li>
+			<li><div><a href="slideshow.html">Slideshow</a></div></li>
 			<li><div>jQuery UI</div>
 				<ul>
 					<li><div><a href="accordion.html">Accordion</a></div></li>
